a8608834794c9815a65b4bfa2390957c78293b8c
jacethelefty <jacemontoya@live.com>
2015-02-19 14:23:23 -0800

    Merge Conflict Update

--- app.js
+++ app.js
@@ -1,66 +1,4 @@
-<<<<<<< HEAD
-=======
-/* DISPLAY ENTER GAME SCREEN  */
-
-  $('body').load(function(){
-    showPopUpBox();
-  });
-
-/* PREVENT NUMBERS LESS THAN 1, GREATER THAN 10, AND NON-DIGITS IN INPUT FILEDS */
-/* CODE COPIED FROM http://jsfiddle.net/94ayn8b6/4/ ON FEB 13TH 2015 */
-
-  $(".txt.input-sm").keydown(function (event) {
-  //prevent using shift with numbers
-  if (event.shiftKey == true) {
-    event.preventDefault();
-  }
-
-  if (!((event.keyCode == 190) || (event.keyCode >= 48 && event.keyCode <= 57) 
-    || (event.keyCode >= 96 && event.keyCode <= 105) || event.keyCode == 8 || event.keyCode == 9 
-    || event.keyCode == 37 || event.keyCode == 39 || event.keyCode == 46)) {
-    event.preventDefault();
-    }
-  });
-
-  $(".txt.input-sm").keyup(function (event) {
-    var number = parseFloat($(this).val());
-  if(number > 10 || number < 1){
-    $(this).val("");
-    }
-  });
-
- /* HIDE ELEMENT FUNCTIONS */
-
-function hideEntry1(){
-  var hide1 = document.getElementById('guess1').style.color;
-  if (hide1 == "white"){
-    document.getElementById("guess1").style.color="black";
-  }else{
-    document.getElementById("guess1").style.color="white";
-  }     
-}
-
-function hideEntry2(){
-  var hide2 = document.getElementById('guess2').style.color;
-  if (hide2 == "white"){
-    document.getElementById("guess2").style.color="black";
-  }else{
-    document.getElementById("guess2").style.color="white";
-  }     
-}
-
-function hidePopUpBox(){
-  var pop = document.getElementById('popUpBox').style.display;
-  if (pop == "none"){
-    document.getElementById("popUpBox").style.display="block";
-  }else{
-    document.getElementById("popUpBox").style.display="none";
-  }   
-}
-
 /* CALCULATE GAME RESULTS FUNCTION */
-
->>>>>>> FETCH_HEAD
 function guessBattle(){
   var answer, guess1, guess2;
 
@@ -133,7 +71,6 @@ function guessBattle(){
 function refreshPage() {		
   window.location.reload(true);
 }
-<<<<<<< HEAD
 
 /* PREVENT NUMBERS LESS THAN 1, GREATER THAN 10, AND NON-DIGITS IN INPUT FILEDS */
 /* CODE COPIED FROM http://jsfiddle.net/94ayn8b6/4/ ON FEB 13TH 2015 */
@@ -165,5 +102,4 @@ function refreshPage() {
       $(this).val("");
     }
   });
-=======
->>>>>>> FETCH_HEAD
+
